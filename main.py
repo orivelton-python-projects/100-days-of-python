7818ffb9c9ffa8ddf190b237351721546be488bc
Orivelton Cesar <orivelton.cesar@intellisense.io>
2021-11-22 16:01:19 +0000

    chore: Input test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello " + input("What is your name?"))
+# print("Hello " + input("What is your name?"))
+# num_char = len("Hello")
+# new_num_char = str(num_char)
+# print(type(num_char))
+
+# a = 123
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+print("You have " + str(int(age) * 365) + " days" )
